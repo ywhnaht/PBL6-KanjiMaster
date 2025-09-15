c3c571b4bb390507576e4fb4d182ff2ef903fae8
Huy Thanh <hothanhhuy24062004@gmail.com>
2025-09-15 21:07:20 +0700

    fixed port

--- backend/src/main/java/com/kanjimaster/backend/config/WebConfig.java
+++ backend/src/main/java/com/kanjimaster/backend/config/WebConfig.java
@@ -31,7 +31,7 @@ public class WebConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5137"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
