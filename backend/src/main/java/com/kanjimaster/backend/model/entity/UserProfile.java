c57ef4dd185e1ee322e1b252cb150717c8f70f48
ywhnaht <hothanhhuy24062004@gmail.com>
2025-10-23 13:18:01 +0700

    Add fullname to RegisterDto, created Profile when register successfully

--- backend/src/main/java/com/kanjimaster/backend/model/entity/UserProfile.java
+++ backend/src/main/java/com/kanjimaster/backend/model/entity/UserProfile.java
@@ -10,7 +10,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
 @EntityListeners(AuditingEntityListener.class)
 @Table(name = "user_profiles")
 @AllArgsConstructor
