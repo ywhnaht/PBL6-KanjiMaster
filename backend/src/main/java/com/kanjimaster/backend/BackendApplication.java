4d5fbe23f2ebcbf42cb55e37c5eadcdcafa6a097
ywhnaht <hothanhhuy24062004@gmail.com>
2025-10-24 22:24:10 +0700

    set default timezone UTC

--- backend/src/main/java/com/kanjimaster/backend/BackendApplication.java
+++ backend/src/main/java/com/kanjimaster/backend/BackendApplication.java
@@ -1,13 +1,22 @@
 package com.kanjimaster.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.util.TimeZone;
+
 @EnableWebMvc
+@EnableAsync
 @SpringBootApplication
 public class BackendApplication {
 
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
