e91cc6bddb7af7544bcd5ab364337d8b42087668
ywhnaht <hothanhhuy24062004@gmail.com>
2025-11-07 18:30:37 +0700

    change docker file

--- AI/backend/docker/entrypoints/entrypoint.sh
+++ AI/backend/docker/entrypoints/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+set -e
+
+echo "🚀 Running migrations..."
 python manage.py makemigrations
 python manage.py migrate
-python manage.py test
 
-exec "$@"
+echo "✅ Migrations done, starting Daphne..."
+
+# Chạy Daphne server
+exec daphne -b 0.0.0.0 -p 8000 drf_course_main.asgi:application
