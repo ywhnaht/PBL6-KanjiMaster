6debcdf68efe8f3f83a7bbc9af86a203306418b3
ywhnaht <hothanhhuy24062004@gmail.com>
2025-12-14 14:25:44 +0700

    fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
-  },
   server: {
     proxy: {
       '/api': {
