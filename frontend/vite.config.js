2f7e657b9a6233ae1df3e7252acf2a29a5ae0d87
ywhnaht <hothanhhuy24062004@gmail.com>
2025-12-14 14:47:09 +0700

    fix docker file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,29 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   base: './',
+//   server: {
+//     proxy: {
+//       // Khi frontend gọi /api, Vite sẽ chuyển sang backend
+//       '/api': {
+//         target: 'http://localhost:8080', // backend của bạn
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  base: '/',  // ✅ HOẶC không có dòng base
   server: {
     proxy: {
       '/api': {
